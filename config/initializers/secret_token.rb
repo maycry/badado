8cf11933d7ed8b136e72b45acf52e1fc30288a94
maycry <byorickq@gmail.com>
2011-09-11 12:37:55 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Badado::Application.config.secret_token = '1f032ca7ce8fa9c4b3e68c801769b072f981c93cf32eb317e950c9f87e4a78a3e72221e8560725865737e640a0b14d49690a054daa6ae87f79333cd3b10aad4c'
